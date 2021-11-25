5272b28fe711746e2e1536cd43d93e4fad4d5266
hgshin <hyeongyun32@gmail.com>
2021-11-25 13:27:20 +0900

    step3

--- 00_ssh.tf
+++ 00_ssh.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "hgshin_key" {
-    key_name = "hgshin-key"
-    public_key = file("../../.ssh/hgshin-key.pub")
+  key_name   = "hgshin-key"
+  public_key = file("../../.ssh/hgshin-key.pub")
 }
